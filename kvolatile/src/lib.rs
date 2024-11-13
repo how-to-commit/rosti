60ab13f55c94d07df23695d3f235479370b768fa
how-to-commit <52496366+how-to-commit@users.noreply.github.com>
2024-11-13 17:31:24 +0800

    chore: simplify KVolatile type

--- kvolatile/src/lib.rs
+++ kvolatile/src/lib.rs
@@ -4,40 +4,66 @@ use core::marker::PhantomData;
 use core::ptr::{read_volatile, write_volatile, NonNull};
 
 #[repr(transparent)]
-pub struct KVolatile<'a, T>
-where
-    T: ?Sized + Copy,
-{
-    pointer: NonNull<T>,
-    _marker: PhantomData<&'a T>,
-}
+pub struct KVolatile<T: Copy>(T);
 
-impl<'a, T> KVolatile<'a, T>
-where
-    T: ?Sized + Copy,
-{
-    pub const fn new(pointer: NonNull<T>) -> KVolatile<'a, T> {
-        let _marker = PhantomData;
-        KVolatile { pointer, _marker }
+impl<T: Copy> KVolatile<T> {
+    pub const fn new(value: T) -> KVolatile<T> {
+        KVolatile(value)
     }
 
     pub fn read(&self) -> T {
-        unsafe { read_volatile(self.pointer.as_ptr()) }
+        unsafe { read_volatile(&self.0) }
     }
 
-    pub fn write(&self, value: T) {
-        unsafe { write_volatile(self.pointer.as_ptr(), value) }
+    pub fn write(&mut self, value: T) {
+        unsafe { write_volatile(&mut self.0, value) }
     }
 }
+// #[repr(transparent)]
+// pub struct KVolatile<'a, T>
+// where
+//     T: ?Sized + Copy,
+// {
+//     pointer: NonNull<T>,
+//     _marker: PhantomData<&'a T>,
+// }
+
+// impl<'a, T> KVolatile<'a, T>
+// where
+//     T: ?Sized + Copy,
+// {
+//     pub const fn new(pointer: NonNull<T>) -> KVolatile<'a, T> {
+//         let _marker = PhantomData;
+//         KVolatile { pointer, _marker }
+//     }
+
+//     pub fn read(&self) -> T {
+//         unsafe { read_volatile(self.pointer.as_ptr()) }
+//     }
+
+//     pub fn write(&self, value: T) {
+//         unsafe { write_volatile(self.pointer.as_ptr(), value) }
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    // #[test]
+    // fn basic_test() {
+    //     let value = 42;
+    //     let wrapped_value = KVolatile::new(NonNull::from(&value));
+    //     assert_eq!(wrapped_value.read(), 42);
+
+    //     wrapped_value.write(54);
+    //     assert_eq!(wrapped_value.read(), 54);
+    // }
+
     #[test]
     fn basic_test() {
         let value = 42;
-        let wrapped_value = KVolatile::new(NonNull::from(&value));
+        let mut wrapped_value = KVolatile::new(value);
         assert_eq!(wrapped_value.read(), 42);
 
         wrapped_value.write(54);
