b7be5be15ed1f3a8ffbe60545d7142e77d55db2b
JL <52496366+how-to-commit@users.noreply.github.com>
2025-02-18 19:10:55 +0800

    feat: add more gdt stuff

--- kernel/src/gdt.rs
+++ kernel/src/gdt.rs
@@ -1,10 +1,27 @@
+#![allow(dead_code)]
+use core::arch::asm;
+
+#[repr(C, packed)]
+struct GdtTable {
+    limit: u16,
+    base: u32,
+}
+
+impl GdtTable {
+    unsafe fn read_from_sgdt() -> Self {
+        let mut ret = core::mem::MaybeUninit::uninit();
+        asm!(r#"sgdt ({})"#, in (reg) ret.as_mut_ptr(), options(att_syntax, nostack, preserves_flags));
+        ret.assume_init()
+    }
+}
+
 #[repr(u8)]
-enum AccessByteItems {
+enum AccessByte {
     Accessed = 1,
     ReadWrite = 2,
     Direction = 4,
     Executable = 8,
-    DescriptorType = 16,
+    NotSystemDescriptor = 16,
     Present = 128,
 
     Privilege0 = 0,
@@ -13,17 +30,44 @@ enum AccessByteItems {
     Privilege3 = 96,
 }
 
-struct Flag {
+struct GdtSegment {
     access_byte: u8,
-    flag: u8,
+    flag: u8, // higher nibble is used
+    base: u32,
+    limit: u32,
 }
 
-impl Flag {
-    fn generate_repr(self) -> u16 {
+impl GdtSegment {
+    fn new(base: u32, limit: u32) -> Self {
+        Self {
+            access_byte: 0,
+            flag: 0,
+            base,
+            limit,
+        }
+    }
+
+    /// modified from: osdev wiki
+    /// https://wiki.osdev.org/GDT_Tutorial
+    fn as_u64(self) -> u64 {
+        let mut desc = (self.limit & 0x000f_0000) as u64;
+        desc |= (self.flag as u64) << 16 & 0x00f0_0000;
+        desc |= (self.access_byte as u64) << 8 & 0x0000_ff00;
+        desc |= (self.base as u64) >> 16 & 0x0000_00ff;
+        desc |= self.base as u64 & 0xff00_0000;
+
+        desc = desc << 32;
+        desc |= (self.base as u64) << 16;
+        desc |= (self.limit & 0x0000_ffff) as u64;
+
+        desc
+    }
+
+    fn flag_as_u16(self) -> u16 {
         (self.flag << 8) as u16 | self.access_byte as u16
     }
 
-    fn with_access_byte(mut self, item: AccessByteItems) -> Self {
+    fn with_access_byte(mut self, item: AccessByte) -> Self {
         self.access_byte ^= item as u8;
         self
     }
@@ -70,7 +114,7 @@ impl Flag {
             // from osdev wiki:
             // "For any other type of segment (other code types or any data segment), it should be clear (0)."
             // What does "any other type of segment" mean? There are other code types??
-            if self.access_byte & (AccessByteItems::Executable as u8) == 0 {
+            if self.access_byte & (AccessByte::Executable as u8) == 0 {
                 return Err("cannot set long mode on non-executable segment");
             }
 
@@ -82,17 +126,35 @@ impl Flag {
         Ok(self)
     }
 }
-/// modified from: osdev wiki
-/// https://wiki.osdev.org/GDT_Tutorial
-fn create_gdt_descriptor(base: u32, limit: u32, flag: u16) -> u64 {
-    let mut desc = (limit & 0x000f_0000) as u64;
-    desc |= (flag << 8) as u64 & 0x00f0_ff00;
-    desc |= (base >> 16) as u64 & 0x0000_00ff;
-    desc |= base as u64 & 0xff00_0000;
 
-    desc = desc << 32;
-    desc |= (base << 16) as u64;
-    desc |= (limit & 0x0000_ffff) as u64;
+fn create_gdt_entries() -> [u64; 3] {
+    let code = GdtSegment::new(0, 0xffff_ffff)
+        .with_access_byte(AccessByte::Present)
+        .with_access_byte(AccessByte::NotSystemDescriptor)
+        .with_access_byte(AccessByte::Executable)
+        .with_access_byte(AccessByte::Accessed)
+        .set_32bit_segment_size(true)
+        .set_page_granularity(true)
+        .as_u64();
 
-    desc
+    let data = GdtSegment::new(0, 0xffff_ffff)
+        .with_access_byte(AccessByte::Present)
+        .with_access_byte(AccessByte::NotSystemDescriptor)
+        .with_access_byte(AccessByte::ReadWrite)
+        .with_access_byte(AccessByte::Accessed)
+        .set_32bit_segment_size(true)
+        .set_page_granularity(true)
+        .as_u64();
+
+    let blank = GdtSegment::new(0, 0).as_u64();
+
+    [blank, code, data]
+}
+
+fn init_gdt() {
+    let entries = create_gdt_entries().to_vec().leak();
+    let ptr = entries.as_ptr();
+
+    // TODO: init the table, write the address to gdtr
+    unimplemented!()
 }
