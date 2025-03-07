b8cb542ccd4043a298c3cb871e23d65c84a67632
how-to-commit <52496366+how-to-commit@users.noreply.github.com>
2025-03-08 00:25:47 +0800

    fix: broken memcmp implementation

--- src/libc.rs
+++ src/libc.rs
@@ -1,8 +1,8 @@
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn memset(ptr: *mut u8, value: u8, len: usize) -> *mut u8 {
+pub unsafe extern "C" fn memset(ptr: *mut u8, value: i32, len: usize) -> *mut u8 {
     for i in 0..len {
         unsafe {
-            *ptr.add(i) = value;
+            *ptr.add(i) = value as u8;
         }
     }
     ptr
@@ -20,22 +20,15 @@ pub unsafe extern "C" fn memcpy(dst: *mut u8, src: *mut u8, len: usize) -> *mut 
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn memcmp(lhs: *const u8, rhs: *const u8, count: usize) -> i32 {
-    if lhs == rhs {
-        return 0;
-    }
-
     for i in 0..count {
-        let cmp = lhs.cmp(&rhs) as i32;
-        if cmp != 0 {
-            return cmp;
-        }
-
-        // increment counter with .add
         unsafe {
-            let _ = lhs.add(i);
-            let _ = rhs.add(i);
+            let a = *lhs.add(i) as u8;
+            let b = *rhs.add(i) as u8;
+
+            if a != b {
+                return (a - b) as i32;
+            }
         }
     }
-
     0
 }
